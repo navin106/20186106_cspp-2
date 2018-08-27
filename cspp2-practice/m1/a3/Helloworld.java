47d378606c7e7685743cff480f0eb9ff8afeb85a
navin106 <sandula.navin@msitprogram.net>
2018-08-28 03:45:32 +0530

    helloworld

---
+++ cspp2-practice/m1/a3/Helloworld.java
@@ -0,0 +1,17 @@
+/**.
+* first program
+*/
+public final class Helloworld {
+    /**.
+    * private construcor
+    **/
+    private Helloworld() {
+    }
+    /**.
+    * @param args is for command line arguments
+    *
+    */
+    public static void main(final String[] args) {
+        System.out.println(2*3.0);
+    }
+}
---
+++ cspp2-practice/m1/a3/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Helloworld","l":"main(String[])","url":"main-java.lang.String:A-"}]
