c060865d25dde99f9bc06d75a09e15ee0558113b
navin106 <sandula.navin@msitprogram.net>
2018-08-29 05:27:56 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -1,33 +1,35 @@
 import java.util.Scanner;
+/**
+*@author:navin106.
+*/
+public final class Solution {
+    /**
+    *Do not modify this main function.
+    */
+    private Solution() {
 
-public class Solution {
-	/**
-	*Do not modify this main function.
-	*/
-	private Solution() {
-
-	}
-	/**
-	*@param args for command line input
-	*/
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/**
-	Need to write the rootsOfQuadraticEquation \
-	function and print the output.
-	*/
-	/**
+    }
+    /**
+    *@param args for command line input
+    */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /**
+    Need to write the rootsOfQuadraticEquation \
+    function and print the output.
+    */
+    /**
     *Need to write the rootsOfQuadraticEquation function and print the output.
     *@param x int
     *@param y int
     *@param z int
     */
-    public static void rootsOfQuadraticEquation(final int x,final int y, final int z) {
+    public static void rootsOfQuadraticEquation(final int x, final int y, final int z) {
         double d = (y * y) - (2 * 2 * x * z);
         double qua = Math.sqrt(d);
         double r1 = ((-y + qua) / (2 * x));
