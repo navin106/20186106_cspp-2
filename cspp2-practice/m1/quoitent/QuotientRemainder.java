e45100b672dc3e49ee797d50558fe0a920095b01
navin106 <sandula.navin@msitprogram.net>
2018-08-28 15:49:34 +0530

    quotient

---
+++ cspp2-practice/m1/quoitent/QuotientRemainder.java
@@ -0,0 +1,23 @@
+/**
+*date = 21-08-2018.
+*name = navin106.
+*swap class.
+*/
+public final class QuotientRemainder {
+    /**
+    * private constructor.
+    */
+    private QuotientRemainder() {
+
+    }
+    /**
+    * @param args for command line argument.
+    */
+    public static void main(final String[] args) {
+        int a = 38;
+        int b = 3;
+        float q = a / b;
+        float r = a % b;
+    System.out.println("q =" + q + "," + "r =" + r);
+    }
+}
