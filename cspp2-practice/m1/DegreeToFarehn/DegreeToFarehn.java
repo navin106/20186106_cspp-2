a564a3e22f163849701ad3a4401a01bd722f4232
navin106 <sandula.navin@msitprogram.net>
2018-08-28 16:15:09 +0530

    degree to farehn

---
+++ cspp2-practice/m1/DegreeToFarehn/DegreeToFarehn.java
@@ -0,0 +1,19 @@
+/**
+*name: navin106.
+date:21-08-2018.
+*/
+public final class DegreeToFarehn {
+    /**
+    *constructor.
+    */
+    private DegreeToFarehn() {
+
+    }
+    /**@param args command line arguments*/
+    public static void main(final String[] args) {
+        final int degree = 10;
+        final int farehn = degree + 32;
+        System.out.println("degree =" + degree + "farehn =" + farehn);
+    }
+
+}
