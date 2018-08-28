2a26ed066d4d2a667f17dfada6ac413e7a01d25e
navin106 <sandula.navin@msitprogram.net>
2018-08-29 00:03:41 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);      
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        System.out.println((long)Math.pow(base, exponent));
+	}
+	/*
+	Need to write the power function and print the output.
+	*/
+}
