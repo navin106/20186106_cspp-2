c808c9df22f6052ca3f665b4fa4f86168346f7b6
navin106 <sandula.navin@msitprogram.net>
2018-08-29 00:04:16 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
 	Do not modify this main function.
 	*/
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+		Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
         System.out.println((long)Math.pow(base, exponent));
