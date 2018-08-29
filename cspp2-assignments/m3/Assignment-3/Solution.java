d40549d120314bcc63ba870d7ad7f974ced52b9f
navin106 <sandula.navin@msitprogram.net>
2018-08-29 19:36:00 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+/**
+	Do not modify this main function.
+	*/
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);      
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        int gcd = gcd(n1,n2);
+        System.out.println(gcd);
+    }
+    /**
+	Need to write the gcd function and print the output.
+	*/
+    public static int gcd(final int n1,final int n2) {
+        if (n1 == 0 || n2 == 0) {
+            return 0;
+        }
+        if (n1 == n2) {
+            return n1;
+        }
+        if (n1 > n2) 
+            return gcd(n1 - n2, n2);
+        return gcd(n1, n2 - n1);
+    }
+}
