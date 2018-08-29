7c053875ee483f390c91f73b0881674add3b8c6f
navin106 <sandula.navin@msitprogram.net>
2018-08-29 19:40:57 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -32,8 +32,10 @@ public class Solution {
         if (n1 == n2) {
             return n1;
         }
-        if (n1 > n2) 
+        if (n1 > n2) {
             return gcd(n1 - n2, n2);
-        return gcd(n1, n2 - n1);
+        } else {
+            return gcd(n1, n2 - n1);
+        }
     }
 }
