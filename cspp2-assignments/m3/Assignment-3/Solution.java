fda4c914131ef3b9f53e1453096de31541b36276
navin106 <sandula.navin@msitprogram.net>
2018-08-29 19:40:15 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -1,21 +1,30 @@
 
 import java.util.Scanner;
 /**
-	Do not modify this main function.
-	*/
+    Do not modify this main function.
+    @author : navin106
+    */
 public class Solution {
+/**
+*constructor.
+*/
+    private Solution() {
 
+    }
+    /**
+    *@param args 
+    */
     public static void main(String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        int gcd = gcd(n1,n2);
+        int gcd = gcd(n1, n2);
         System.out.println(gcd);
     }
     /**
-	Need to write the gcd function and print the output.
-	*/
+    Need to write the gcd function and print the output.
+    */
     public static int gcd(final int n1,final int n2) {
         if (n1 == 0 || n2 == 0) {
             return 0;
