48eb9093b6bd6132f3dff921453bd8b9691d9b56
navin106 <sandula.navin@msitprogram.net>
2018-08-31 01:51:07 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,33 +1,99 @@
 import java.util.Scanner;
-import java.util.Arrays;
 /**
- * @author:navin106
+ * @author : navin106
  */
-public class Solution {
+
+public final class Solution {
 	/**
 	 * Constructs the object.
 	 */
 	private Solution() {
 
 	}
-	/* Fill the main function to print resultant of addition of matrices*/
 	/**
-	 * main method to add the two matrices.
+	 * { function_description }
 	 *
 	 * @param      args  The arguments
 	 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		String k = s.nextLine();
-		int[] l = new int[k.length()];
-		int i = 0;
-		for (String a : k) {
-			l[i] = Integer.parseInt(a);
-			i++;
-		}
-		System.out.println(l);
+	public static void main(final String[] args) {
+		Scanner inp = new Scanner(System.in);
+		int a = inp.nextInt();
+		int b = inp.nextInt();
+		int[][] m1 = buildmat(a, b);
+		int x = inp.nextInt();
+		int y = inp.nextInt();
+		int[][] m2 = buildmat(x, y);
+		addmat(m1, m2);
 	}
-	public static int  mat(int a, int b) {
-		return 0;
+	/**
+	 * { function_description }
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      b     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public static int[][] buildmat(final int a,final int b) {
+		Scanner inp1 = new Scanner(System.in);
+		int[][] k = empmatrix(a, b);
+		for (int i = 0; i < a; i++) {
+			for (int j = 0;j < b; j++) {
+				k[i][j] = inp1.nextInt();
+			}
+		}
+		return k;
+	}
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      m1    The m 1
+	 * @param      m2    The m 2
+	 */
+	public static void addmat(final int[][] m1,final int[][] m2) {
+		int rowm1 = m1.length;
+		int rowm2 = m2.length;
+		int colm1 = m1[0].length;
+		int colm2 = m2[0].length;
+		if (rowm1 == rowm2  && colm1 == colm2) {
+			int[][] empmatadd = buildmat(rowm1, colm1);
+			for (int i = 0; i < rowm1; i++) {
+				for (int j = 0; j < colm1; j++) {
+					empmatadd[i][j] = m1[i][j] + m2[i][j];
+				}
+			}
+		System.out.println(matout(empmatadd));
+		} else {
+			System.out.println("not possible");
+		}
+
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      b     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public static int[][] empmatrix(final int a,final int b) {
+		return new int[a][b];
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      resmat  The resmat
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public static String matout(final int[][] resmat) {
+		String mat = "";
+		for (int i = 0; i < resmat.length; i++) {
+			for (int j = 0; j < resmat[0].length; j++) {
+				mat += resmat[i][j] + " ";
+			}
+			mat = mat.trim();
+			mat +="\n";
+		}
+		return mat;
 	}
 }
