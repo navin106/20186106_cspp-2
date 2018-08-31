f0e25fe39790c84f7de9bec7258f5c13d5b1e71a
navin106 <sandula.navin@msitprogram.net>
2018-08-31 11:37:51 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -3,30 +3,45 @@ import java.lang.Math;
 import java.util.Scanner;
 // Given an String, Write a java method that returns the decimal value for the given binary string.
 public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.nextLine();
-		for (int i = 0; i < n; i++) {
-			String s = sc.nextLine();
-			String res = binaryToDecimal(s); //Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	public static String binaryToDecimal(String s) {
-		int i = s.length() - 1;
-		int sum = 0;
-		String k = " ";
-		for (char a : s.toCharArray()) {
-			if (a == '1') {
-				sum += Math.pow(2, i);
-			}
-			i--;
-		}
-		k = k + sum;
-		return k.trim();
-	}
+    /*
+    Do not modify this main function.
+    */
+    private Solution() {
+
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s); //Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
+    /**
+     * { function_description }
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String binaryToDecimal(String s) {
+        int i = s.length() - 1;
+        int sum = 0;
+        String k = " ";
+        for (char a : s.toCharArray()) {
+            if (a == '1') {
+                sum += Math.pow(2, i);
+            }
+            i--;
+        }
+        k = k + sum;
+        return k.trim();
+    }
 }
