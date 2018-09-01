424c18e1389d0833953d053dea7f9f3d66f368c2
navin106 <sandula.navin@msitprogram.net>
2018-09-01 11:47:05 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -25,26 +25,22 @@ public final class Solution {
 		for (int i = 1; i <= n ; i++ ) {
 			cnt = 0;
 			for (int j = 1; j <= i ; j++ ) {
+				if (j * j == i) {
+					cnt -= 2;
+				} else {
 					if (i % j == 0) {
 						cnt += 1;
-				}else {
-					if (j*j == i){
-						cnt -=2;
 					}
 				}
 
-
 			}
-			// System.out.println(cnt);
 			if (cnt == 4) {
 				a[l] = i;
 				l++;
 			}
 
 		}
-		// System.out.println("---------");
-		for (int k= 0;k<a.length ;k++) {
-			// System.out.println(a[k]);
+		for (int k= 0;k<a.length ;k++ ) {
 			if (a[k] != 0 && a[k]%2 !=0) {
 				System.out.println(a[k]);
 			}
