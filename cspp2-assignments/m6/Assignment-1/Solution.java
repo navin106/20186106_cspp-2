210de80f263d4ac1873f0eb3ed8e9e32e3d518be
navin106 <sandula.navin@msitprogram.net>
2018-09-01 12:00:36 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -25,8 +25,8 @@ public final class Solution {
 		for (int i = 2; i <= n; i++ ) {
 			cnt = 0;
 			for (int j = 2; j <= Math.sqrt(i); j++) {
-				if (i%j == 0) {
-					cnt +=1;
+				if (i % j == 0) {
+					cnt += 1;
 				}
 			}
 			if (cnt != 0) {
