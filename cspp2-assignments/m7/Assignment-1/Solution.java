40826176bd0aa81b128293be9c668149b76a261e
navin106 <sandula.navin@msitprogram.net>
2018-09-03 20:14:16 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
  * @author: navin106.
  */
 public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { item_description }.
-	 * @param : args
-	 */
-	public static void main(final String args[]) {
+    }
+    /**
+     * { item_description }.
+     * @param args
+     */
+    public static void main(final String args[]) {
 
-		Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
-		String input = s.next();
+        String input = s.next();
 
-		InputValidator i = new InputValidator(input);
+        InputValidator i = new InputValidator(input);
 
-		System.out.println(i.validateData());
-	}
+        System.out.println(i.validateData());
+    }
 }
