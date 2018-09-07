ff15c87d836f2d64430b7551cfdbbb42951fe917
navin106 <sandula.navin@msitprogram.net>
2018-09-07 12:26:25 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-1/List.java
+++ cspp2-assignments/m11/Assignment-1/List.java
@@ -375,7 +375,7 @@ public class List {
                     l.removeAll(a);
                 }
                 break;
-            case "subList": {
+            case "subList": 
                 if (tokens.length != 2) {
                     break;
                 }
@@ -386,7 +386,7 @@ public class List {
                     System.out.println(object);
                 }
                 break;
-            }
+            
             case "equals":
                 if (tokens.length == 2) {
                     String[] lt = tokens[1].split(",");
