8e3b63235b3e512448d2cf24702b25acf2d01270
navin106 <sandula.navin@msitprogram.net>
2018-09-07 17:54:54 +0530

    "check_style_score -> 8 of 8 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-2/List.java
+++ cspp2-assignments/m11/Assignment-2/List.java
@@ -166,7 +166,7 @@ public class List<E> {
 	public boolean contains(E item) {
 		//Write logic for contains method
 		for (int i = 0; i < size; i++) {
-			if (list[i] == item) {
+			if (list[i].equals(item)) {
 				return true;
 			}
 		}
