a2b7c2a71ae0cbc2e4e2ea515439e48b974b2f6d
navin106 <sandula.navin@msitprogram.net>
2018-09-11 19:52:43 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m14/SortedSet/Solution.java
+++ cspp2-assignments/m14/SortedSet/Solution.java
@@ -1,94 +1,86 @@
 import java.util.Scanner;
 import java.util.Arrays;
-/**
- * @author :navin106
- * date:sep-11- 2018.
- */
-class Sortedset extends Set{
-    public int last() {
-        return adtlist[size - 1];
-    }
-    public int[] addAll(int[] a) {
-        int[] b = new int[a.length];
-        int k = 0;
-        for (int i = 0; i < a.length; i++) {
-            if(!contains(a[i])) {
-                b[k++] = a[i];
-            }
-        }
-        return b;
-    }
-    public int[] headSet(int toElement) {
-        int len = 0;
-        for (int j =0; j<size; j++) {
-            if(adtlist[j] == toElement) {
-                len = j;
-                break;
-            }
-        }
-        int[] headSet = new int[len];
-        for (int i =0; i<len; i++) {
-            if (adtlist[i] < toElement) {
-                headSet[i] = adtlist[i];
-            }
-        }
-        return headSet;
-    }
-    public int[] subSet(int fromElement, int toElement) {
-        int len = 0;
-        for (int j =0; j<size; j++) {
-            if(adtlist[j] == toElement) {
-                len = j;
-                break;
-            }
-        }
-        int k = 0;
-        int[] subSet = new int[len];
-        for (int i=0; i<len; i++) {
-            if (adtlist[i] == fromElement) {
-                subSet[k++] = adtlist[i];
-            }
-            
-        }
-        return subSet;
-    }
-
+class SortedSet extends Set {
+	public int[] subSet(int fromElement, int toElement) {
+		if (fromElement > toElement) {
+			System.out.println("Invalid​ ​ Arguments​ ​ to​ Subset​ ​ Exception");
+			return null;
+		}
+		int index = Arrays.asList(adtlist).indexOf(fromElement);
+		int[] subSet = new int[size - index];
+		int k = 0;
+		for (int i = index; i < size; i++) {
+			if (adtlist[i] < toElement) {
+				subSet[k++] = adtlist[i];
+			}
+		}
+		return subSet;
+	}
+	public int[] headSet(int toElement) {
+		if (toElement == adtlist[0]) {
+			System.out.println("Invalid​ ​ Arguments​ ​ to​ Subset​ ​ Exception");
+			return null;
+		}
+		int index = Arrays.asList(adtlist).indexOf(toElement);
+		int[] headSet =  new int[size - index];
+		int k = 0;
+		for (int i = 0; i < index; i++) {
+			headSet[k++] = adtlist[i];
+		}
+		return headSet;
+	}
+	public int last() {
+		if (size == 0) {
+			System.out.println("Set​ Empty​ Exception");
+			return -1;
+		}
+		return adtlist[size - 1];
+	}
+	public void addAll(int[] d) {
+		// Set k = new Set();
+		if (d.length > 0) {
+			for (int a : d) {
+				add(a);
+			}
+		}
+		return;
+	}
 }
-public final class Solution {
-    public static void main(String[] args) {
-        Sortedset s =  new Sortedset();
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            String[] token = (sc.next()).split(" ");
-            switch (token[0]) {
-            case "last":
-                System.out.println(s.last());
-                break;
-            case "addAll":
-                if (token[1].length() > 0 ) {
-                    String[] a = token[1].split(",");
-                    int[] b = new int[a.length];
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		SortedSet b = new SortedSet();
+		while (sc.hasNext()) {
+			String[] token = sc.next().split(" ");
+			switch (token[0]) {
+			case "subSet":
+				String[] a = token[1].split(",");
+				System.out.println(b.subSet( Integer.parseInt(a[0]), Integer.parseInt(a[1])));
+				break;
+			case "headSet":
+				System.out.println(b.headSet(Integer.parseInt(token[1])));
+				break;
+			case "last":
+				System.out.println(b.last());
+				break;
+			case "addAll":
+				String[] d = token[1].split(",");
+				int[] c = new int[d.length];
+				int i = 0;
+				for (String e : d) {
+					c[i++] = Integer.parseInt(e.trim());
+				}
+				b.addAll(c);
+				break;
+			case "print":
+				System.out.println(b);
+				break;
+			default:
+				break;
+			}
 
-                    for (int i = 0; i < a.length; i++) {
-                        b[i] = Integer.parseInt(a[i]);
-                    }
-                    System.out.println(s.addAll(b));
-                    
-                }
-                break;
-            case "print":
-                System.out.println(s.toString());
-                break;
-            case "headSet":
-                System.out.println(s.headSet(Integer.parseInt(token[1])));
-                break;
-            case "subSet":
-                String[] c = token[1].split(",");
-                System.out.println(s.subSet(Integer.parseInt(c[0]), Integer.parseInt(c[1])));
-                break;
-            default:
-                break;
-            }
-        }
-    }
+
+		}
+
+	}
 }
