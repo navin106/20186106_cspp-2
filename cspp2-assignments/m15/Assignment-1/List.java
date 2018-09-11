b4172801a9550e50ac7c01f677f378398046d18e
navin106 <sandula.navin@msitprogram.net>
2018-09-11 10:46:40 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-1/List.java
+++ cspp2-assignments/m15/Assignment-1/List.java
@@ -266,14 +266,12 @@ public class List {
                 for (int i = start; i < end; i++) {
                     k.add(list[i]);
                 }
+                return k;
             // }
         }
     catch(Exception e) {
         System.out.println("Index Out of Bounds Exception");
-    }
-    finally {
-
-        return k;
+        return null;
     }
     }
     /**
