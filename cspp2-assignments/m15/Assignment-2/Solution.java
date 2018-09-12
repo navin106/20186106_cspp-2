9b77012e6005801d3cacde1e547de0675d61210a
navin106 <sandula.navin@msitprogram.net>
2018-09-13 00:43:26 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-2/Solution.java
+++ cspp2-assignments/m15/Assignment-2/Solution.java
@@ -209,9 +209,9 @@ public final class Solution {
             case "intersection":
                 SortedSet s = new SortedSet();
                 SortedSet t = new SortedSet();
-                intArray = intArray((token[1].replace("[","").replace("]","")).split(","));
+                intArray = intArray(((token[1].replace("[","")).replace("]","")).split(","));
                 s.addAll(intArray);
-                intArray = intArray((token[2].replace("[","").replace("[","")).split(","));
+                intArray = intArray(((token[2].replace("[","")).replace("[","")).split(","));
                 t.addAll(intArray);
                 int[] z = (s.intersection(t)).adtlist;
                           String str = "{";
@@ -223,9 +223,9 @@ public final class Solution {
                 break;
             case "retainAll":
                 s = new SortedSet();
-                intArray = intArray((token[1].replace("[","").replace("]","")).split(","));
+                intArray = intArray(((token[1].replace("[","")).replace("]","")).split(","));
                 s.addAll(intArray);
-                intArray = intArray((token[2].replace("[","").replace("]","")).split(","));
+                intArray = intArray(((token[2].replace("[","")).replace("]","")).split(","));
                 System.out.println(s.retainAll(intArray));
                 break;
             default:
