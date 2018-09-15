41c64f10e6844e7d22cad2271678d3be8c281dd2
navin106 <sandula.navin@msitprogram.net>
2018-09-15 12:25:26 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -192,11 +192,11 @@ public final class Solution {
 		for (int i = 1; i <= size; i++) {
 			System.out.println("question text " + i);
 			if (quizmarklist[i-1].getmarkaward() > 0) {
-				System.out.println(" Correct Answer! - Marks Awarded:" + quizlist[i].getmaxmark());
-				mark += quizlist[i-1].getmaxmark();
+				System.out.println(" Correct Answer! - Marks Awarded:" + quizlist[i].getmarkaward());
+				mark += quizlist[i-1].getmarkaward();
 			} else {
-				System.out.println(" Wrong Answer! - Marks Awarded:" + quizlist[i].getnegativemark());
-				mark += quizlist[i-1].getnegativemark();
+				System.out.println(" Wrong Answer! - Penalty:" + quizlist[i].getmarkaward());
+				mark += quizlist[i-1].getmarkaward();
 			}
 		}
 		System.out.println("Total Score: " + mark);
