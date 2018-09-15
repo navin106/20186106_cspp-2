761efae20ddfd863f13eea586686d0501f3db459
navin106 <sandula.navin@msitprogram.net>
2018-09-15 12:42:37 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -177,7 +177,7 @@ public final class Solution {
 
 		}
 		for (int k = 0; k < qsize; k++) {
-			System.out.println("question text 1(" + quizmarklist[k].getmarkaward	() + ")");
+			System.out.println("question text "+(k+1)+"(" + quizmarklist[k].getmarkaward	() + ")");
 			System.out.println("choice 1	choice 2	choice 3	choice 4");
 			System.out.println();
 		}
