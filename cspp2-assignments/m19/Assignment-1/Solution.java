b2e80300da12edabbca16d5ac5e548b5655afca9
navin106 <sandula.navin@msitprogram.net>
2018-09-17 10:33:05 +0530

    "check_style_score -> 2 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -181,7 +181,7 @@ public final class Solution {
 				String[] avail = quizlist[i].getchoices();
 				for (int j = 0; j < avail.length; j++) {
 					// if (choose.equals(avail[j])) {
-					if (choose.equals(avail[quizlist[i].getrtanswer()])) {
+					if (choose.equals(avail[quizlist[i].getrtanswer() - 1])) {
 						quizmarklist[size++] = new Quiz(quizlist[i].getmaxmark(), k[1]);
 					} else {
 						quizmarklist[size++] = new Quiz(quizlist[i].getnegativemark(), k[1]);
