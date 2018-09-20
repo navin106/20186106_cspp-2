ddbad09778faa8b9911dfb0e14aebb2265d086d1
navin106 <sandula.navin@msitprogram.net>
2018-09-21 05:00:19 +0530

    project plagirism

--- cspp2-assignments/m22/Solution.java
+++ cspp2-assignments/m22/Solution.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Arrays;
 class SociaNetwork {
 	HashMap<String, ArrayList<String>> sn;
 	SociaNetwork() {
---
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Collection;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+class Freqdict {
+	HashMap<String, Integer> hm;
+	Freqdict() {
+		hm = new HashMap<String, Integer>();
+	}
+	HashMap<String, Integer> makedict(String[] d) {
+		HashSet<String> set = new HashSet<>(Arrays.asList(d));
+		int cnt = 0;
+		for (String l : set) {
+			cnt = 0;
+			for (String k : d) {
+				if (l.equals(k)) {
+					cnt++;
+				}
+			}
+			hm.put(l, cnt);
+		}
+		return hm;
+	}
+}
+class Plagarise {
+	int Dotproduct(HashMap<String, Integer> freqd1, HashMap<String, Integer> freqd2) {
+		int dp = 0;
+		HashSet<String> newSet = new HashSet<String>(freqd1.keySet());
+
+		newSet.addAll(freqd2.keySet());
+
+		for (String k : newSet) {
+			if (freqd1.get(k) == null) {
+				freqd1.put(k, 0);
+			}
+			if (freqd2.get(k) == null) {
+				freqd2.put(k, 0);
+			}
+			dp += freqd1.get(k) * freqd2.get(k);
+		}
+		return dp;
+	}
+	double EuclideanNorm(Collection<Integer> a, Collection<Integer> b) {
+		int s1 = 0;
+		int s2 = 0;
+		for (int c : a) {
+			s1 += Math.pow(c, 2);
+		}
+		for (int d : b) {
+			s2 += Math.pow(d, 2);
+		}
+		return Math.sqrt(s1) * Math.sqrt(s2);
+	}
+}
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		Freqdict k1;
+		Plagarise pl = new Plagarise();
+		Scanner sc = new Scanner(System.in);
+		File folder = new File(sc.next());
+		File[] filesArray = folder.listFiles();
+		ArrayList<String> filestringslist = new ArrayList<String>();
+		for (File a : filesArray) {
+			FileReader f = new FileReader(a);
+			BufferedReader b = new BufferedReader(f);
+			String filestring = "";
+			while (b.readLine() != null) {
+				filestring += b.readLine();
+			}
+			filestringslist.add(filestring);
+			f.close();
+			b.close();
+		}
+		HashMap<String, Integer> freqd1;
+		double percent = 0;
+		ArrayList<Integer> resultlist = new ArrayList<Integer>();
+		ArrayList<HashMap<String, Integer>> dictlist = new ArrayList<HashMap<String, Integer>>();
+		for (String b : filestringslist) {
+			k1 = new Freqdict();
+			freqd1 = new HashMap<String, Integer>();
+			freqd1 = k1.makedict(cleanstring(b.toLowerCase().replace(".", " ")).split(" "));
+			dictlist.add(freqd1);
+		}
+		for (HashMap<String, Integer> k : dictlist) {
+			for (HashMap<String, Integer> l : dictlist) {
+				int dp = pl.Dotproduct(k, l);
+				double en = pl.EuclideanNorm(k.values(), l.values());
+				percent = dp / en * 100;
+				resultlist.add((int) percent);
+			}
+		}
+		String[] filenames = folder.list();
+		String s = "" + "\t" + "\t";
+		for (String k : filenames) {
+			s += k + "\t";
+		}
+		int k = 0;
+		s += "\n";
+		for (int i = 0; i < filenames.length; i++) {
+			s += filenames[i] + "\t";
+			for (int j = 0; j < filenames.length; j++) {
+				s += resultlist.get(k++) + "\t" +"\t";
+			}
+			s += "\n";
+		}
+		System.out.println(s);
+	}
+	public static String cleanstring(String d1) {
+		Pattern p = Pattern.compile("[^a-z 0-9]");
+		Matcher m = p.matcher(d1);
+		String newstring = m.replaceAll("");
+		return newstring;
+	}
+
+}
